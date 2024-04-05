7d51679510c5f34d9ec807a096c435fb7cfd7a48
Krishnanand A <krishnanand654@gmail.com>
2024-04-06 01:06:51 +0530

    token verification

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const authenticateUser = (req, res, next) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
     if (!user) {
-        return res.status(401).json({ message: 'Invalid username and passsword' });
+        return res.status(401).json({ message: 'Invalid username or password' });
     }
     req.user = user;
     next();
@@ -34,7 +34,28 @@ const authenticateUser = (req, res, next) => {
 app.post('/login', authenticateUser, (req, res) => {
     const token = jwt.sign(req.user, SECRET_KEY);
     res.json({ token });
+});
+
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(403).json({ message: 'Token is required' });
+    }
+
+    jwt.verify(token.split(' ')[1], SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid token' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+app.get('/', verifyJWT, (req, res) => {
+    res.status(200).json({ message: 'ok' });
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
